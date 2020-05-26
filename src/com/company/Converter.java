82179158624f26fd3f0c8c7801a0b2ff472a1e1a
arina <dudkina.arin@yandex.ru>
2020-05-26 21:24:39 +0500

    Initial commit

---
+++ src/com/company/Converter.java
@@ -0,0 +1,33 @@
+package com.company;
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class Converter {
+
+    public void convert(File in, String codIn, File out, String codOut) throws Exception {
+
+        InputStream is = new FileInputStream(in);
+        OutputStream off = new FileOutputStream(out);
+        byte [] buffer = new byte[is.available()];
+        is.read(buffer, 0, is.available());
+
+        Charset codI = Charset.forName(codIn);
+        Charset codO = Charset.forName(codOut);
+
+        String s = new String(buffer, codI);
+        buffer = s.getBytes(codO);
+
+        off.write(buffer, 0, buffer.length);
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        File file = new File ("C://Users//dudki//IdeaProjects//Convector//myFile2.txt");
+        File file1 = new File("C://Users//dudki//IdeaProjects//Convector//myFile7.txt");
+        Converter con = new Converter();
+        con.convert(file, "UTF-8", file1, "cp1251");
+    }
+
+
+}

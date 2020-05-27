7e604207abaed19add6327f29955a86eb6b80e68
arina <dudkina.arin@yandex.ru>
2020-05-27 22:53:05 +0500

    try catch

--- src/com/company/Converter.java
+++ src/com/company/Converter.java
@@ -5,28 +5,31 @@ import java.nio.charset.Charset;
 public class Converter {
 
     public void convert(File in, String codIn, File out, String codOut) throws Exception {
+        try {
+            InputStream is = new FileInputStream(in);
+            OutputStream off = new FileOutputStream(out);
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer, 0, is.available());
 
-        InputStream is = new FileInputStream(in);
-        OutputStream off = new FileOutputStream(out);
-        byte [] buffer = new byte[is.available()];
-        is.read(buffer, 0, is.available());
+            Charset codI = Charset.forName(codIn);
+            Charset codO = Charset.forName(codOut);
 
-        Charset codI = Charset.forName(codIn);
-        Charset codO = Charset.forName(codOut);
+            String s = new String(buffer, codI);
+            buffer = s.getBytes(codO);
 
-        String s = new String(buffer, codI);
-        buffer = s.getBytes(codO);
-
-        off.write(buffer, 0, buffer.length);
+            off.write(buffer, 0, buffer.length);
+        }catch (UnsupportedEncodingException ex){
+            ex.printStackTrace();
+        }
 
 
     }
 
     public static void main(String[] args) throws Exception {
-        File file = new File ("C://Users//dudki//IdeaProjects//Convector//myFile2.txt");
-        File file1 = new File("C://Users//dudki//IdeaProjects//Convector//myFile7.txt");
+        File file = new File ("C://Users//dudki//IdeaProjects//Convector//myFile.txt");
+        File file1 = new File("C://Users//dudki//IdeaProjects//Convector//myFile2.txt");
         Converter con = new Converter();
-        con.convert(file, "UTF-8", file1, "cp1251");
+        con.convert(file, "UTF-8", file1, "ISO8859-5");
     }
 
 
